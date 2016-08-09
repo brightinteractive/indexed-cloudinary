b66718f67796e4f47fea9211fb938afaa64cb10c
Angus Goldsmith <--global>
2016-08-09 16:12:28 +0100

    Reformated demo code.

--- demo/demo.js
+++ demo/demo.js
@@ -7,38 +7,38 @@ var wallpaperFilters = {category: 'tenerife', suitableForWallpaper: 'true'};
 var cloudName = "hj7ggro7s";
 
 $(document).ready(function () {
-    var queryString = indexedCloudinary.objectToQueryString(filters);
-    indexedCloudinary.displayCarousel('#carousel', {
-            indexHost: indexHost,
+  var queryString = indexedCloudinary.objectToQueryString(filters);
+  indexedCloudinary.displayCarousel('#carousel', {
+      indexHost: indexHost,
       indexAuth: indexAuth,
       index: 'newimages',
-            queryString: queryString,
-            cloudName: cloudName,
-            transformationOptions: {
-                width: 600,
-                height: 400,
-                crop: 'fill',
-                quality: 80
-            }
-        }
-    );
+      queryString: queryString,
+      cloudName: cloudName,
+      transformationOptions: {
+        width: 600,
+        height: 400,
+        crop: 'fill',
+        quality: 80
+      }
+    }
+  );
 
 
-    var wallpaperQueryString = indexedCloudinary.objectToQueryString(wallpaperFilters);
-    indexedCloudinary.changeWallpaper('#credit', {
-            indexHost: indexHost,
+  var wallpaperQueryString = indexedCloudinary.objectToQueryString(wallpaperFilters);
+  indexedCloudinary.changeWallpaper('#credit', {
+      indexHost: indexHost,
       indexAuth: indexAuth,
       index: 'newimages',
-            queryString: wallpaperQueryString,
-            cloudName: cloudName,
-            transformationOptions: {
-                quality: 80
-            },
-            ratingsUrl: 'http://hw-ratings-dev.herokuapp.com'
-        }
-    ).then(function (succesful) {
-        if (succesful) {
-            console.log("Wallpaper changed!")
-        }
-    });
+      queryString: wallpaperQueryString,
+      cloudName: cloudName,
+      transformationOptions: {
+        quality: 80
+      },
+      ratingsUrl: 'http://hw-ratings-dev.herokuapp.com'
+    }
+  ).then(function (succesful) {
+    if (succesful) {
+      console.log("Wallpaper changed!")
+    }
+  });
 });
