6bfe19e01efac9de6635a4ad83023f4f4575a46e
Angus Goldsmith <--global>
2016-11-21 12:30:10 +0000

    Fixed sort order. Was desc should have been asc.

--- src/js/es-client.js
+++ src/js/es-client.js
@@ -9,7 +9,7 @@ export default class ESClient {
   queryStringSearch({ q, index }) {
     const query = encodeURIComponent(q);
 
-    return fetch(`${this.config.host}/${index}/_search?q=${query}&sort=carouselOrder:desc`, {
+      return fetch(`${this.config.host}/${index}/_search?q=${query}&sort=carouselOrder:asc`, {
       cors: true,
       headers: new Headers({
         Authorization: `Basic ${btoa(this.config.auth)}`,
